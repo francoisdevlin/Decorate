938a51f41f3cd2ad4fa45a0480d20d307640ae50
Sean <devlinsf@devlinsolutions.com>
2011-11-17 15:37:24 -0500

    Fixed bug in project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject org.clojars.sean_devlin/decorators "0.0.1-SNAPSHOT"
   :description "A decorator library for Clojure, inspired by Python."
-  :dependencies [[org.clojure/clojure ["1.2.0" "1.3.0"]]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
