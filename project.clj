91b7c11c5b06813e808581d42138b99360a67499
Sean <devlinsf@devlinsolutions.com>
2011-11-17 08:44:48 -0500

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject decorators "0.0.1-alpha"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject org.clojars.sean_devlin/decorators "0.0.1-SNAPSHOT"
+  :description "A decorator library for Clojure, inspired by Python."
+  :dependencies [[org.clojure/clojure ["1.2.0" "1.3.0"]]])
